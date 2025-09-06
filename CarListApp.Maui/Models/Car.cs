1ce30eff5ecb23478da0304a5a298f99343f4695
pawel975 <pawel.kurek975@gmail.com>
2025-09-06 19:01:44 +0200

    Dodaj pliki projektów.

---
+++ CarListApp.Maui/Models/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace CarListApp.Maui.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Vin { get; set; }
+    }
+
+}
